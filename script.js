fb3a859dc7c3c918fab0e54bfcb0544303d4c8f3
Dwayne Fonville <d.fonville18@gmail.com>
2020-06-18 18:06:30 -0400

    add table edit time

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#CurrentDate").append(now);
 // Time Blocks
 for (let i = 5; i <= 22; i++) {
     let time = moment(i, "HH:mm").format("h:mm a");
-  
+    
     let classColor;
     let value;
   
@@ -15,4 +15,21 @@ for (let i = 5; i <= 22; i++) {
       value = "";
     }
 
-    //
+// Time table
+$("#planner-table").append(`
+    <div class="row">
+        <div class="hour 18 col">${time}</div>
+        <textarea id = ${time} class="textarea col ${classColor}">${value}</textarea>
+        <button class="saveBtn col"><i class="far fa-save"></i></button>
+     </div>
+    `);
+}
+
+$(document).on("click", ".saveBtn", function () {
+  let id = $(this).prev().attr("id");
+  let task = $(this).prev().val();
+
+  localStorage.setItem(id, task);
+});
+}
+
